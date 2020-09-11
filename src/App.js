ebdad81805b7389c477da7ddb57e8376a4f8fe42
JaturyLab <jaturonmoonjan@gmail.com>
2020-09-11 09:42:46 +0700

    Commit Add.js Round3

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
 
+const word = "Hello";
 function App() {
-  return (
-    <div>
-    <CharacterCard value="h"/>
-    <CharacterCard value="i"/>
-    </div>
-  );
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
+ );
 }
 
 export default App;

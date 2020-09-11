01eb315f5507c57ad339c458752bf6b77c863aa1
JaturyLab <jaturonmoonjan@gmail.com>
2020-09-11 10:32:39 +0700

    Commit CharacterCard.js  And App.css   Round6

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,8 +1,15 @@
-import React from 'react';
+
 import './App.css';
+import React, { useState } from 'react';
 export default function CharacterCard(props) {
- return (
-    
-    <div className="card">{props.value}</div>
- )
-}
+    const [active, setActive] = useState(false);
+    const activate = () => {
+    setActive(true)
+    }
+    const className = `card ${active ? 'activeCard': ''}`
+    return (
+    <div className={className} onClick={activate}>{props.value}</div>
+    )
+   
+   }
+   

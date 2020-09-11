47d54132eb59603482a2702b9d205298ae1720fb
JaturyLab <jaturonmoonjan@gmail.com>
2020-09-11 10:18:45 +0700

    Commit Add.jsAndCharacterCard.jsAndApp.css Round4

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
 
+
+
 const word = "Hello";
 function App() {
+
  return (
- <div>
+ <div >
  {
  Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
  }
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import './App.css';
 export default function CharacterCard(props) {
  return (
-    <div>{props.value}</div>
+    
+    <div className="card">{props.value}</div>
  )
 }

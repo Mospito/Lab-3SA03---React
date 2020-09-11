bf6adf97569d12c14d528424e0d14baa7e305a31
JaturyLab <jaturonmoonjan@gmail.com>
2020-09-11 11:01:54 +0700

    Commit CharacterCard.js  And WordCard.js   Round7

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -3,9 +3,18 @@ import './App.css';
 import React, { useState } from 'react';
 export default function CharacterCard(props) {
     const [active, setActive] = useState(false);
+
     const activate = () => {
-    setActive(true)
+    
+        if(!active){
+            setActive(true)
+            props.activationHandler(props.value)
+            }
+        
     }
+
+
+    
     const className = `card ${active ? 'activeCard': ''}`
     return (
     <div className={className} onClick={activate}>{props.value}</div>
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import CharacterCard from './CharacterCard'
+
+
+const activationHandler = c => { console.log(`${c} has been activated.`) } 
 export default function WordCard(props){
+    
+    
+
  return (
- <div>
-{ Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
- </div>
+     
+
+    <div>
+        
+    { Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler}/>) }
+    </div>
  );
 }

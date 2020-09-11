782c4188f339251f8599764f0b828e587223fc3e
JaturyLab <jaturonmoonjan@gmail.com>
2020-09-11 09:28:14 +0700

    Commit App.js Round1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CharacterCard from './CharacterCard';
+
+function App() {
+  return (
+    <div>
+    Hello World
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CharacterCard.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default function CharacterCard(props) {
+ return (
+    <div>{props.value}</div>
+ )
+}
